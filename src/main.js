7367d3c23605dd519ef7f357d9f22cfb064c253d
Elias Blomberg <144891485+pregamer189@users.noreply.github.com>
2025-01-23 11:02:21 +0000

    i testar nr 5

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import CurrentResult from './components/CurrentResult.vue'
-import router from './router'
+import router from './router/index.js' // Ensure the correct path to the router file
 
 const app = createApp(App)
 app.component('CurrentResult', CurrentResult)
